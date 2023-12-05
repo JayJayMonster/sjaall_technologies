bf386c449061d85340369fbc3dde3f5ce1b3c2aa
Jay <jayvanherk@gmail.com>
2023-12-05 13:35:43 +0100

    first commit, nuxt init

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col justify-center items-center h-screen">
+    <div class="">
+      Dit is jouw kleur:
+    </div>
+    <div class="">
+      #FFFFFF
+    </div>
+  </div>
+</template>
